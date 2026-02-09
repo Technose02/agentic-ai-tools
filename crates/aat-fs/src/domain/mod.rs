8de67cae3067fa302657f03671b2f12159ac68b0
technose02 <technose02@yahoo.de>
2026-02-09 06:27:04 +0100

    more notes

--- crates/aat-fs/src/domain/mod.rs
+++ crates/aat-fs/src/domain/mod.rs
@@ -2,4 +2,6 @@ pub mod model;
 pub mod port;
 pub mod service;
 
-// TODO: Make Inports generic over Input and Output types as well as the error-type (all associated types)
+// TODO: Make Inports generic over Input and Output types as well as the error-type (all associated types) 
+// implementing error should not depend an AdkError, String is a better target
+// make language a general marker type (e.g. "struct DE;") to keep it consistent through layers (add it to generic functions, traits etc.)
